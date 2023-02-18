0906c05879e63c18e67f2d67364e83f6f6fda2d0
Erika <erika.d.azevedo@hotmail.com>
2023-02-18 18:04:12 -0300

    add página categoria de produtos

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,14 +4,17 @@ import {
   BrowserRouter,
 } from "react-router-dom";
 import Home from "./pages/Home";
+import ProductCategory from "./pages/ProductCategory";
 import ProductContent from "./pages/ProductContent";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <WrapperRoutes>
-        <Route /*path="/" element={<Home />} *//>
+        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<ProductCategory/>}/>
         <Route path="/" element={<ProductContent />} />
+        
       </WrapperRoutes>
 
     </BrowserRouter>
---
+++ src/pages/ProductCategory/index.tsx
@@ -0,0 +1,16 @@
+import './style.css'
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+
+function ProductCategory (){
+    return (
+        <div>
+            <Header/>
+            <h1>Categoria de Produtos</h1>
+            <Footer/>
+        </div>
+    )
+}
+
+export default ProductCategory;
