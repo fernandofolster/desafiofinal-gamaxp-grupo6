fbd14d2217231d2536db1058b68cc6983ed8cef4
Erika <erika.d.azevedo@hotmail.com>
2023-03-07 15:46:02 -0300

    criado pasta api fake para teste de funções

--- src/components/Card-produtos/index.tsx
+++ src/components/Card-produtos/index.tsx
@@ -1,178 +1,11 @@
 import SutiaPreto from "../../assets/images/lingerie-preta.jpg";
 import { CardProductStyled } from "./styled";
 import { useEffect, useState } from "react";
+import { data } from "../apiFake/apiFake";
 
 export function CardProductArea() {
-  const ecommerce = [
-    {
-      nome: "Produto 1",
-      produto_id: "Prod_1",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 1,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 2",
-      produto_id: "Prod_2",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 2,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 3",
-      produto_id: "Prod_3",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 3,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 4",
-      produto_id: "Prod_4",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 1,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 5",
-      produto_id: "Prod_5",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 2,
-      preco: 5,
-      quantidade: 100,
-    },
-
-    {
-      nome: "Produto 6",
-      produto_id: "Prod_6",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 3,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 7",
-      produto_id: "Prod_7",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 1,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 8",
-      produto_id: "Prod_8",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 2,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 9",
-      produto_id: "Prod_9",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 3,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 10",
-      produto_id: "Prod_10",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 1,
-      preco: 5,
-      quantidade: 100,
-    },
-
-    {
-      nome: "Produto 11",
-      produto_id: "Prod_11",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 2,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 12",
-      produto_id: "Prod_12",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 3,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 13",
-      produto_id: "Prod_13",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 1,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 14",
-      produto_id: "Prod_14",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 2,
-      preco: 5,
-      quantidade: 100,
-    },
-    {
-      nome: "Produto 15",
-      produto_id: "Prod_15",
-      foto: SutiaPreto,
-      descricao:
-        "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
-      categoria: "sutiã",
-      categoria_id: 3,
-      preco: 5,
-      quantidade: 100,
-    },
-  ];
-
+  const {ecommerce} = data;
+  
   const categories = [
     {
       categoria: "sutiã",
@@ -183,9 +16,12 @@ export function CardProductArea() {
     {
       categoria: "pijama",
     },
+    {
+      categoria: "all",
+    },
   ];
 
-  const [category, setCategory] = useState(categories)
+  const [category, setCategory] = useState(categories);
 
   const [pagination, setPagination] = useState(10);
   const loadMore = () => {
@@ -203,19 +39,17 @@ export function CardProductArea() {
   return (
     <div className="category-container">
       <CardProductStyled>
-      <div className="category-filter-wrapper">
-        {category.map((category) => {
-          return (
-            
+        <div className="category-filter-wrapper">
+          {category.map((category) => {
+            return (
               <button
                 className="category-filter-btn"
                 onClick={() => filterResult(category.categoria)}
               >
                 {category.categoria}
               </button>
-            
-          );
-        })}
+            );
+          })}
         </div>
 
         <section className="prod-card-wrapper">
---
+++ src/components/apiFake/apiFake.jsx
@@ -0,0 +1,174 @@
+import SutiaPreto from "../../assets/images/lingerie-preta.jpg";
+
+
+export const data = {
+    ecommerce: [
+      {
+        nome: "Produto 1",
+        produto_id: "Prod_1",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 1,
+        preco: 25,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 2",
+        produto_id: "Prod_2",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 2,
+        preco: 35,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 3",
+        produto_id: "Prod_3",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 3,
+        preco: 45,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 4",
+        produto_id: "Prod_4",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 1,
+        preco: 25,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 5",
+        produto_id: "Prod_5",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 2,
+        preco: 35,
+        quantidade: 100,
+      },
+
+      {
+        nome: "Produto 6",
+        produto_id: "Prod_6",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 3,
+        preco: 45,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 7",
+        produto_id: "Prod_7",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 1,
+        preco: 25,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 8",
+        produto_id: "Prod_8",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 2,
+        preco: 35,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 9",
+        produto_id: "Prod_9",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 3,
+        preco: 45,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 10",
+        produto_id: "Prod_10",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 1,
+        preco: 25,
+        quantidade: 100,
+      },
+
+      {
+        nome: "Produto 11",
+        produto_id: "Prod_11",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 2,
+        preco: 35,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 12",
+        produto_id: "Prod_12",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 3,
+        preco: 45,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 13",
+        produto_id: "Prod_13",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 1,
+        preco: 25,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 14",
+        produto_id: "Prod_14",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 2,
+        preco: 35,
+        quantidade: 100,
+      },
+      {
+        nome: "Produto 15",
+        produto_id: "Prod_15",
+        foto: SutiaPreto,
+        descricao:
+          "Descrição do produto: Calcinha Hot Pant Tule Chica Capeto com recortes forrados na parte frontal e posterior em tule estampado pink. ",
+        categoria: "sutiã",
+        categoria_id: 3,
+        preco: 45,
+        quantidade: 100,
+      },
+    ],
+  };
